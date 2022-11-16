94dbccbdb4dce04bb8bd61f9c25bd24a7a13256d
Kristy Vanatta <kristylvanatta@gmail.com>
2022-11-16 10:51:27 -0500

    updating repo

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -38,7 +38,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product ------->????????
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -47,7 +47,7 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  await Product.create(req.body)
+  Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -70,9 +70,9 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', async (req, res) => {
+router.put('/:id', (req, res) => {
   // update product data
-  await Product.update(req.body, {
+  Product.update(req.body, {
     where: {
       id: req.params.id,
     },
